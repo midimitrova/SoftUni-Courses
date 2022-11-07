481798a4ed4b01817792b6533758f6d155396986
midimitrova <mariyapopova95@gmail.com>
2022-11-07 12:05:26 +0200

    Update collect method, moving while loop out of the class

--- Programming Fundamentals with Python/Dictionaries/Exercise/p02_a_miner_task.py
+++ Programming Fundamentals with Python/Dictionaries/Exercise/p02_a_miner_task.py
@@ -1,19 +1,12 @@
 class MinerTask:
 
-    def __init__(self, current_resource, current_quantity):
-        self.current_resource = current_resource
-        self.current_quantity = current_quantity
+    def __init__(self):
         self.collection = {}
 
-    def collect(self):
-        while True:
-            if self.current_resource not in self.collection.keys():
-                self.collection[self.current_resource] = 0
-            self.collection[self.current_resource] += self.current_quantity
-            self.current_resource = input()
-            if self.current_resource == 'stop':
-                break
-            self.current_quantity = int(input())
+    def collect(self, current_resource, current_quantity):
+        if current_resource not in self.collection.keys():
+            self.collection[current_resource] = 0
+        self.collection[current_resource] += current_quantity
 
     def __repr__(self):
         result = ''
@@ -22,8 +15,11 @@ class MinerTask:
         return result
 
 
-resource = input()
-quantity = int(input())
-miner_task = MinerTask(resource, quantity)
-miner_task.collect()
+miner_task = MinerTask()
+while True:
+    resource = input()
+    if resource == 'stop':
+        break
+    quantity = int(input())
+    miner_task.collect(resource, quantity)
 print(miner_task)
