b17df7539b6238883332214062a4dedea542394c
midimitrova <mariyapopova95@gmail.com>
2023-01-25 23:04:06 +0200

    Add Error Handling - Exercise

---
+++ Python Advanced Module/Python Advanced/Error_Handling/Exercise/p01_numbers_dictionary.py
@@ -0,0 +1,33 @@
+numbers_dictionary = {}
+
+while True:
+    line = input()
+    if line == 'Search':
+        break
+    number_as_string = line
+    try:
+        number = int(input())
+        numbers_dictionary[number_as_string] = number
+    except ValueError:
+        print("The variable number must be an integer")
+
+line = input()
+
+while line != "Remove":
+    searched = line
+    print(numbers_dictionary[searched])
+    line = input()
+
+line = input()
+
+while line != "End":
+    searched = line
+    try:
+        del numbers_dictionary[searched]
+    except KeyError:
+        print("Number does not exist in dictionary")
+    line = input()
+
+print(numbers_dictionary)
+
+
