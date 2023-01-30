6b900944e97b067d6da044540c0b353688399b1e
midimitrova <mariyapopova95@gmail.com>
2023-01-30 21:20:45 +0200

    Update problem, found IndexError.

--- Python Advanced Module/Python Advanced/Error_Handling/Exercise/p02_email_validator.py
+++ Python Advanced Module/Python Advanced/Error_Handling/Exercise/p02_email_validator.py
@@ -19,14 +19,16 @@ valid_domains = [".com", ".bg", ".org", ".net"]
 email = input()
 
 while email != "End":
-
-    if "@" not in email:
-        raise MustContainAtStSymbolError("Email must contain @")
-    elif len(findall(pattern_name, email)[0]) <= 4:
-        raise NameRooShortError("Name must be more than 4 characters")
-    elif findall(pattern_domain, email)[0] not in valid_domains:
-        raise InvalidDomainError("Domain must be one of the following: .com, .bg, .org, .net")
-    else:
-        print("Email is valid")
+    try:
+        if "@" not in email:
+            raise MustContainAtStSymbolError("Email must contain @")
+        elif len(findall(pattern_name, email)[0]) <= 4:
+            raise NameRooShortError("Name must be more than 4 characters")
+        elif findall(pattern_domain, email)[0] not in valid_domains:
+            raise InvalidDomainError("Domain must be one of the following: .com, .bg, .org, .net")
+        else:
+            print("Email is valid")
+    except IndexError:
+        print('Invalid email!')
 
     email = input()
