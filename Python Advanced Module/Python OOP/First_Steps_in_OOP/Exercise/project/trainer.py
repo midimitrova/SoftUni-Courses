4c067c985367d0df6c3234d98fa49c9bc6b3a5f7
midimitrova <mariyapopova95@gmail.com>
2023-02-09 22:36:15 +0200

    Add First Steps in OOP - Exercise

---
+++ Python Advanced Module/Python OOP/First_Steps_in_OOP/Exercise/project/trainer.py
@@ -0,0 +1,31 @@
+from project.pokemon import Pokemon
+
+
+class Trainer:
+    def __init__(self, name):
+        self.name = name
+        self.pokemons = []
+
+    def add_pokemon(self, pokemon: Pokemon):
+        if any([x.name == pokemon.name for x in self.pokemons]):
+            return f"This pokemon is already caught"
+        else:
+            self.pokemons.append(pokemon)
+            return f"Caught {pokemon.name} with health {pokemon.health}"
+
+    def release_pokemon(self, pokemon_name):
+        for pokemon in self.pokemons:
+            if pokemon.name == pokemon_name:
+                self.pokemons.remove(pokemon)
+                return f"You have released {pokemon_name}"
+            else:
+                return f"Pokemon is not caught"
+
+    def trainer_data(self):
+        result = ''
+        result += f"Pokemon Trainer {self.name}\n"
+        result += f"Pokemon count {len(self.pokemons)}\n"
+        for pokemon in self.pokemons:
+            result += f"- {pokemon.name} with health {pokemon.health}\n"
+
+        return result
