d038f14f2fdc4a100c4b2be8e0a793e52cd6de32
midimitrova <mariyapopova95@gmail.com>
2023-05-26 19:22:10 +0300

    Preparing Templates, Rendering Queried Data in the Template, Rendering the Details Page, Model URLs, Adding a Slugfield & Overwriting Save, Using the Slug & Updating Field Options, Aggregation & Ordering

--- Python Web/Python Web Basics/Models_in_Django/book_store/book_store/book_outlet/urls.py
+++ Python Web/Python Web Basics/Models_in_Django/book_store/book_store/book_outlet/urls.py
@@ -1,3 +1,8 @@
-urlpatterns = (
+from django.urls import path
 
+from book_store.book_outlet.views import index, book_detail
+
+urlpatterns = (
+    path('', index, name='index'),
+    path('<slug:slug>/', book_detail, name='book-detail'),
 )
